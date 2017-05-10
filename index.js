bedf3d034aa1ac749b30beced9a5b1cfdbfd1295
cornholio <0@mcornholio.ru>
2017-05-10 10:57:34 +0300

    Pushing state to history

--- index.js
+++ index.js
@@ -4,12 +4,33 @@ var button = document.getElementById('go');
 var daFrame = document.getElementById('da-frame');
 var urlInput = document.getElementById('url-input');
 
-button.addEventListener('click', function () {
-    daFrame.src = urlInput.value;
-});
+var query = parseQuery(window.location.search);
+
+if (query.url) {
+    urlInput.value = query.url;
+    updateUrl();
+}
+
+function updateUrl() {
+    var url = urlInput.value;
+    daFrame.src = url;
+    window.history.pushState({}, 'url', window.location.pathname.replace(/\/+$/, "") + '?url=' + encodeURIComponent(url));
+}
+
+button.addEventListener('click', updateUrl);
 
 urlInput.addEventListener('keydown', function (event) {
     if (event.keyCode == 13) {
-        daFrame.src = urlInput.value;
+        updateUrl();
     }
 });
+
+function parseQuery(qstr) {
+    var query = {};
+    var a = (qstr[0] === '?' ? qstr.substr(1) : qstr).split('&');
+    for (var i = 0; i < a.length; i++) {
+        var b = a[i].split('=');
+        query[decodeURIComponent(b[0])] = decodeURIComponent(b[1] || '');
+    }
+    return query;
+}
